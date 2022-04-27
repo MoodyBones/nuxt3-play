df13399bde141e4933f02c2c775741aee4234d87
Mel <jones.mel@gmail.com>
2022-04-27 15:47:05 +0200

    add Guessform component to page

--- n3rdle/pages/game.vue
+++ n3rdle/pages/game.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 // const state = [['guess', '00022']]
 // move logic to composables
-const { state } = useGameState()
+const { state, submitGuess } = useGameState()
 </script>
 
 <template>
   <section>
     <h2>Playing game now</h2>
     <GameBoard :state="state" />
+    <GuessForm @guess="submitGuess" />
   </section>
 </template>
 
