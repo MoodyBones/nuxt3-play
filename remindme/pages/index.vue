c798aed4e3429fab7d0be89f96eb38f8a8154a0e
Mel <jones.mel@gmail.com>
2022-07-18 21:38:52 +0200

    refactor: script & template order switcharoo

--- remindme/pages/index.vue
+++ remindme/pages/index.vue
@@ -1,30 +1,3 @@
-<template>
-  <main class="container mx-auto flex flex-col px-4 md:px-12">
-    <TheNav :app-name="appName" />
-    <article>
-      <h2 class="text-sm text-gray-600">
-        Today is {{ isWeekday }}. You have {{ activeNotes }}
-        {{ activeNotes > 1 ? 'todos' : 'todo' }} active. 💪
-        {{ colorMode.value === 'light' ? '🌞' : '🌝' }}
-      </h2>
-    </article>
-    <article class="mx-auto w-full max-w-prose">
-      <ul aria-labelledby="list-summary" class="flex flex-col gap-4 pt-12">
-        <li v-for="item in noteItems" :key="item.id" class="">
-          <NoteItem
-            :id="item.id"
-            :label="item.label"
-            :done="item.done"
-            @checkbox-changed="updateDoneStatus(item.id)"
-            @item-deleted="deleteNote(item.id)"
-            @item-edited="editNote(item.id, $event)"
-          />
-        </li>
-      </ul>
-    </article>
-  </main>
-</template>
-
 <script setup>
 import { useTitle } from '@vueuse/core'
 import uniqueId from 'lodash.uniqueid'
@@ -90,3 +63,30 @@ function editNote(noteId, newLabel) {
   noteToEdit.label = newLabel
 }
 </script>
+
+<template>
+  <main class="container mx-auto flex flex-col px-4 md:px-12">
+    <TheNav :app-name="appName" />
+    <article>
+      <h2 class="text-sm text-gray-600">
+        Today is {{ isWeekday }}. You have {{ activeNotes }}
+        {{ activeNotes > 1 ? 'todos' : 'todo' }} active. 💪
+        {{ colorMode.value === 'light' ? '🌞' : '🌝' }}
+      </h2>
+    </article>
+    <article class="mx-auto w-full max-w-prose">
+      <ul aria-labelledby="list-summary" class="flex flex-col gap-4 pt-12">
+        <li v-for="item in noteItems" :key="item.id" class="">
+          <NoteItem
+            :id="item.id"
+            :label="item.label"
+            :done="item.done"
+            @checkbox-changed="updateDoneStatus(item.id)"
+            @item-deleted="deleteNote(item.id)"
+            @item-edited="editNote(item.id, $event)"
+          />
+        </li>
+      </ul>
+    </article>
+  </main>
+</template>
