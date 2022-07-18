550219af529c0b8b766a9ffead3cd62a029bcb9a
Mel <jones.mel@gmail.com>
2022-07-18 19:12:33 +0200

    refactor: remove dead code

--- remindme/components/TheNav.vue
+++ remindme/components/TheNav.vue
@@ -1,30 +1,23 @@
 <script setup>
-defineProps({
+const props = defineProps({
   appName: String,
 })
-
-const color = useColorMode()
-const toggleDark = () => {
-  color.value = color.value === 'light' ? 'dark' : 'light'
-}
 </script>
 
 <template>
-  <nav class="my-2 flex flex-nowrap items-center justify-between">
-    <NuxtLink
-      to="/"
-      class="text-3xl font-extrabold tracking-tight text-gray-700 dark:text-gray-200"
-    >
-      {{ appName }}
-    </NuxtLink>
-    <ColorModeSwitch />
-    <!-- <button
-      class="rounded-full px-5 py-1.5 bg-gray-200 hover:bg-gray-300 text-xs font-bold uppercase tracking-tight"
-      @click="toggleDark"
-    >
-      <span class="sr-only">Switch theme to:</span>
-      <span v-if="color === 'light'">☽ dark</span>
-      <span v-else>☼ light</span>
-    </button> -->
+  <nav class="my-2">
+    <ul class="flex flex-nowrap items-center justify-between">
+      <li>
+        <NuxtLink
+          to="/"
+          class="text-3xl font-extrabold tracking-tight text-gray-700 dark:text-gray-200"
+        >
+          {{ props.appName }}
+        </NuxtLink>
+      </li>
+      <li>
+        <ColorModeSwitch />
+      </li>
+    </ul>
   </nav>
 </template>
